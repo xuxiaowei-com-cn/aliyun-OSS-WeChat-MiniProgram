0528841c94c50f049f5023741e9fbfc5883fa181
徐晓伟 <xuxiaowei@live.hk>
2019-06-08 12:08:43 +0800

    上传

--- pages/aliyunoss/aliyunoss.js
+++ pages/aliyunoss/aliyunoss.js
@@ -1,4 +1,7 @@
 // pages/aliyunoss/aliyunoss.js
+
+import upload from '../../aliyunoss/upload.js'
+
 Page({
 
   /**
@@ -46,8 +49,59 @@ Page({
   uploadImg: function(e) {
 
     var files = this.data.files;
-    console.log(files)
-  
+
+    //  replace：http://www.w3school.com.cn/jsref/jsref_replace.asp
+    // /：需要转译的字符串
+    // g：替换所有
+    var key = new Date().toLocaleDateString().replace(/\//g, "-") // 上传路径
+
+    for (var i = 0; i < files.length; i++) {
+
+      var chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678' /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+      var maxPos = chars.length
+      var pwd = '' // 随机数，用于文件名
+      for (var j = 0; j < 10; j++) {
+        pwd += chars.charAt(Math.floor(Math.random() * maxPos))
+      }
+
+      var file = files[i]
+
+      var pos = file.lastIndexOf('.')
+      var suffix = '' // 文件后缀名
+      if (pos != -1) {
+        suffix = file.substring(pos)
+      }
+
+      key += "/" + pwd + suffix
+
+      // https://developers.weixin.qq.com/miniprogram/dev/api/network/upload/wx.uploadFile.html
+      wx.uploadFile({
+        url: upload.url, // 必填：是 开发者服务器地址
+        filePath: file, // 必填：是 要上传文件资源的路径
+        name: 'file', // 必填：是 文件对应的 key，开发者在服务端可以通过这个 key 获取文件的二进制内容
+        formData: {
+          name: file,
+          // key: "123/${filename}", // 临时文件名
+          key: key,
+          policy: upload.policy,
+          OSSAccessKeyId: upload.OSSAccessKeyId,
+          success_action_status: upload.success_action_status,
+          signature: upload.signature
+        },
+        success: res => {
+          console.log("上传结果 success：", res)
+          wx.showToast({
+            title: '上传成功',
+            icon: 'success',
+            duration: 2000 // 提示的延迟时间
+          })
+        },
+        fail(res) {
+          console.log("上传结果 fail：", res)
+        }
+      })
+
+    }
   },
 
   /**
